96a3e49250104c7b2f50f719feffae3d06083a99
amribm <ameerthecoder@gmail.com>
2024-09-21 21:37:46 +0530

    Add(setup): day2

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,29 @@
-use clap::{command, Command};
+type Error = Box<dyn std::error::Error>;
+
+use clap::{arg, command, ArgAction, Command};
 
 const EXECUTABLE_NAME: &str = "rs-todo";
 
-fn command() -> Command {
+pub fn command() -> Command {
     command!()
+        .bin_name(EXECUTABLE_NAME)
         .about("Simple Todo app")
-        .subcommand(Command::new("list"))
+        .subcommand(
+            Command::new("add").arg(
+                arg!([TODO])
+                    .help("todos to append")
+                    .action(ArgAction::Append),
+            ),
+        )
+}
+
+struct Todo {
+    todos: Vec<String>,
+    todo_path: String,
+    todo_backup: String,
+    no_backup: String,
+}
+
+impl Todo {
+    fn new() -> Result<Todo, Error> {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use rs_todo::command;
+
 fn main() {
-    println!("simple rust cli todo app");
+    let matches = command().get_matches();
+
+    match matches.subcommand() {
+        Some(("add", sub_matches)) => {
+            let todos = sub_matches
+                .get_many::<String>("TODO")
+                .unwrap_or_default()
+                .map(|v| v.as_str())
+                .collect::<Vec<_>>();
+
+            println!("todos: {:?}", todos)
+        }
+        Some(("list", _)) => {}
+        Some(("rm", _)) => {}
+        Some(("done", _)) => {}
+        // Some(("raw", _)) => {}
+        Some(("edit", _)) => {}
+        Some(("sort", _)) => {}
+        Some(("reset", _)) => {}
+        Some(("restore", _)) => {}
+        _ => println!("hello world!"),
+    }
 }
