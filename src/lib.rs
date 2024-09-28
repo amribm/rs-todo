c9db0b40a70b6bd53850ddf92df1be9407e7a8d4
amribm <ameerthecoder@gmail.com>
2024-09-28 18:40:50 +0530

    Add: Day3

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use clap::{arg, command, ArgAction, Command};
+use colored::*;
 use std::env;
 use std::fs::OpenOptions;
-use std::io::{BufReader, Read};
+use std::io::{stdout, BufReader, BufWriter, Read, Write};
 
 type Error = Box<dyn std::error::Error>;
 
@@ -15,6 +16,24 @@ pub fn command() -> Command {
             Command::new("add").arg(
                 arg!([TODO])
                     .help("todos to append")
+                    .action(ArgAction::Append),
+            ),
+        )
+        .subcommand(Command::new("list"))
+        .subcommand(
+            Command::new("done").arg(
+                arg!([INDEXES])
+                    .help("todos numbers to mark as done")
+                    .action(ArgAction::Append),
+            ),
+        )
+        .subcommand(
+            Command::new("edit").arg(arg!([TODO]).help("todo to edit").action(ArgAction::Append)),
+        )
+        .subcommand(
+            Command::new("remove").arg(
+                arg!([INDEXES])
+                    .help("todos numbers to remove")
                     .action(ArgAction::Append),
             ),
         )
@@ -66,5 +85,127 @@ impl Todo {
             todos: todo,
             no_backup,
         })
+    }
+
+    pub fn list(self) -> Result<(), Error> {
+        let mut buffer = BufWriter::new(stdout());
+
+        let mut data = String::new();
+
+        for (index, line) in self.todos.into_iter().enumerate() {
+            let symbol = &line[..4];
+            let task = &line[4..];
+            if "[ ] " == symbol {
+                data = format!("{}. {}\n", index + 1, task);
+            } else if "[*] " == symbol {
+                data = format!("{}. {}\n", index + 1, task.strikethrough());
+            }
+
+            buffer.write_all(data.as_bytes())?;
+        }
+        Ok(())
+    }
+
+    pub fn add(&mut self, todos: Vec<&str>) -> Result<(), Error> {
+        if todos.is_empty() {
+            return Err("need one or more todos".into());
+        }
+
+        let todofile = OpenOptions::new()
+            .create(true)
+            .read(true)
+            .write(true)
+            .append(true)
+            .open(&self.todo_path)
+            .expect("can't open Todo file");
+
+        let mut buffer = BufWriter::new(todofile);
+
+        for todo in todos {
+            if todo.trim().is_empty() {
+                continue;
+            }
+
+            let line = format!("[ ] {}\n", todo);
+
+            buffer.write_all(line.as_bytes())?;
+        }
+        Ok(())
+    }
+
+    pub fn done(&mut self, indexs: Vec<usize>) -> Result<(), Error> {
+        if indexs.is_empty() {
+            return Err("rs-todo takes atleast one argument".into());
+        }
+
+        for ind in indexs {
+            if self.todos.len() < ind - 1 {
+                return Err(format!("incorrect index: {}", ind).into());
+            }
+            self.todos[ind - 1] = format!("[*] {}", &self.todos[ind - 1][4..])
+        }
+
+        let todo_path = OpenOptions::new().write(true).open(&self.todo_path)?;
+
+        let mut buffer = BufWriter::new(todo_path);
+
+        for todo in self.todos.iter() {
+            let data = format!("{}\n", &todo);
+            buffer.write_all(data.as_bytes())?;
+        }
+
+        Ok(())
+    }
+
+    pub fn edit(&mut self, args: Vec<String>) -> Result<(), Error> {
+        if args.len() < 2 {
+            return Err("edit expects atleast two arguments [INDEX] [TODO]".into());
+        }
+
+        let todo_path = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open(&self.todo_path)?;
+
+        let mut buffer = BufWriter::new(&todo_path);
+
+        for (ind, todo) in self.todos.iter_mut().enumerate() {
+            if !args[0].contains(&(ind + 1).to_string()) {
+                let data = format!("{}\n", todo);
+                buffer.write_all(data.as_bytes())?;
+                continue;
+            }
+
+            let data = format!("{}{}\n", &todo[..4], args[1]);
+
+            buffer.write_all(data.as_bytes())?;
+        }
+
+        Ok(())
+    }
+
+    pub fn remove(&mut self, args: Vec<usize>) -> Result<(), Error> {
+        if args.is_empty() {
+            return Err("remove expects atleast one arguments [INDEX]".into());
+        }
+
+        let todo_path = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open(&self.todo_path)?;
+
+        let mut buffer = BufWriter::new(&todo_path);
+
+        for (ind, todo) in self.todos.iter().enumerate() {
+            if args.contains(&(ind + 1)) {
+                continue;
+            }
+            let data = format!("{}\n", todo);
+
+            println!("args not contains: {}", data);
+            buffer.write_all(data.as_bytes())?;
+        }
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use rs_todo::command;
+use rs_todo::{command, Todo};
 
 fn main() {
     let matches = command().get_matches();
+
+    let mut todo = match Todo::new() {
+        Ok(t) => t,
+        Err(e) => {
+            panic!("unable to run todo manager due to e: {}", e)
+        }
+    };
 
     match matches.subcommand() {
         Some(("add", sub_matches)) => {
@@ -11,15 +18,38 @@ fn main() {
                 .map(|v| v.as_str())
                 .collect::<Vec<_>>();
 
-            println!("todos: {:?}", todos)
+            let _ = todo.add(todos).is_err_and(|e| panic!("error: {}", e));
         }
-        Some(("list", _)) => {}
+        Some(("list", _)) => {
+            let _ = todo.list().is_err_and(|e| panic!("error: {}", e));
+        }
         Some(("rm", _)) => {}
-        Some(("done", _)) => {}
-        // Some(("raw", _)) => {}
-        Some(("edit", _)) => {}
+        Some(("done", sub_matches)) => {
+            let indexes = sub_matches
+                .get_many::<String>("INDEXES")
+                .unwrap_or_default()
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            let _ = todo.done(indexes).is_err_and(|e| panic!("error: {}", e));
+        }
+        Some(("edit", sub_matches)) => {
+            let args = sub_matches
+                .get_many::<String>("TODO")
+                .unwrap_or_default()
+                .map(|x| x.to_string())
+                .collect::<Vec<_>>();
+
+            let _ = todo.edit(args).is_err_and(|e| panic!("error: {}", e));
+        }
         Some(("sort", _)) => {}
-        Some(("reset", _)) => {}
+        Some(("remove", sub_matches)) => {
+            let indexes = sub_matches
+                .get_many::<String>("INDEXES")
+                .unwrap_or_default()
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+            let _ = todo.remove(indexes).is_err_and(|e| panic!("error: {}", e));
+        }
         Some(("restore", _)) => {}
         _ => println!("hello world!"),
     }
